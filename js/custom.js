d2a8be99323ba8301f93cb57498733631b88e868
MiChang <immi5.tw@gmail.com>
2016-07-10 23:19:33 +0800

    內頁基本版

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,24 @@
 $(function () {
-	$(".nav li a[href^='#'], .logo-s a, .logo a").on('click', function (e) {
-		e.preventDefault();
-		var hash = this.hash;
-
-		$('html, body').animate({
-			scrollTop: $(hash).offset().top
-		}, 300, function () {
-			window.location.hash = hash;
-		});
-	});
+//	$(".nav li a[href^='#'], .logo-s a, .logo-big a").on('click', function (e) {
+//		e.preventDefault();
+//		var hash = this.hash;
+//
+//		$('html, body').animate({
+//			scrollTop: $(hash).offset().top
+//		}, 300, function () {
+//			window.location.hash = hash;
+//		});
+//	});
 	var mainbottom = $('#Home').offset().top + $('#Home').height();
 
 	$(window).on('scroll', function () {
 		stop = Math.round($(window).scrollTop());
 		if (stop > mainbottom) {
 			$('.navbar').addClass('past-main');
-			$('h2.logo').addClass('past-logo');
+			$('h2.logo-big').addClass('past-logo');
 		} else {
 			$('.navbar').removeClass('past-main');
-			$('h2.logo').removeClass('past-logo');
+			$('h2.logo-big').removeClass('past-logo');
 		}
 	});
 	$('.nav a').on('click', function () {
